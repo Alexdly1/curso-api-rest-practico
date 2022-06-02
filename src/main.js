05df0d1df75aec2d43624ad9329b2500ee9f3025
alexander <alexanderdly1@gmail.com>
2022-06-02 00:24:00 -0500

    Mostrando y ocultando secciones

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ async function getTrendingMoviesPreview() {
 
   const  movies = data.results;
   movies.forEach(movie => {
-    const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+    const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList')
 
     const movieContainer = document.createElement('div');
     movieContainer.classList.add('movie-container');
@@ -32,7 +32,7 @@ async function getTrendingMoviesPreview() {
     );
     
     movieContainer.appendChild(movieImg);
-    trendingPreviewMoviesContainer.appendChild(movieContainer)
+    trendingMoviesPreviewList.appendChild(movieContainer)
   });
 }
 
@@ -45,7 +45,7 @@ async function getCategoriesPreview() {
   
   const  categories = data.genres;
   categories.forEach(category => {
-    const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
+    const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list')
 
     const categoryContainer = document.createElement('div');
     categoryContainer.classList.add('category-container');
@@ -57,6 +57,6 @@ async function getCategoriesPreview() {
 
     categoryTitle.appendChild(categoryTitleText);
     categoryContainer.appendChild(categoryTitle);
-    previewCategoriesContainer.appendChild(categoryContainer);
+    categoriesPreviewList.appendChild(categoryContainer);
   });
 }

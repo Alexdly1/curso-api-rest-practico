1a285f9429aa00a2d8c97ba1b98ee1ba32a5fdb7
alexander <alexanderdly1@gmail.com>
2022-06-02 19:59:58 -0500

    Endpoint de detalles de una pelÃicula

--- src/main.js
+++ src/main.js
@@ -16,6 +16,9 @@ function createMovies(movies, container) {
   movies.forEach(movie => {
     const movieContainer = document.createElement('div');
     movieContainer.classList.add('movie-container');
+    movieContainer.addEventListener('click', () => {
+      location.hash = '#movie=' + movie.id;
+    });
 
     const movieImg = document.createElement('img');
     movieImg.classList.add('movie-img');
@@ -63,6 +66,7 @@ async function getTrendingMoviesPreview() {
   // const data = await res.json();
 
   const  movies = data.results;
+  console.log(movies);
 
   createMovies(movies, trendingMoviesPreviewList);
   //La funcio reemplaza esta porcion del codigo
@@ -164,5 +168,26 @@ async function getTrendingMovies() {
 
   createMovies(movies, genericSection);
 
+}
+
+async function getMovieById(id) {
+  const { data: movie } = await api('movie/'+ id);
+  
+  const movieImgUrl = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
+  console.log(movieImgUrl);
+  headerSection.style.background = `
+    linear-gradient(
+      180deg, 
+      rgba(0, 0, 0, 0.35) 19.27%, 
+      rgba(0, 0, 0, 0) 29.17%
+    ),
+    url(${movieImgUrl})
+  `;
+
+  movieDetailTitle.textContent = movie.title;
+  movieDetailDescription.textContent = movie.overview;
+  movieDetailScore.textContent = movie.vote_average;
+
+  createCategories(movie.genres, movieDetailCategoriesList);
 }
 
--- src/navigation.js
+++ src/navigation.js
@@ -93,8 +93,12 @@ function movieDetailsPage() {
   categoriesPreviewSection.classList.add('inactive');
   genericSection.classList.add('inactive');
   movieDetailSection.classList.remove('inactive');
-}
 
+  //['#movie=', '2341']
+  const [_, movieId] = location.hash.split('=');
+  getMovieById(movieId);
+}
+  
 function searchPage() {
   console.log('Search!!');
 
